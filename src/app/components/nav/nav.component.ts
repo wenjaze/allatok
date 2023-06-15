ab06a8a047e95f380014d93f59e5a53c97d60901
wenjaze <raska.gergely@gmail.com>
2023-06-15 17:59:35 +0200

    Add base menu-item ui

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'src/app/interfaces/menu-item';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+	selector: 'app-nav',
+	templateUrl: './nav.component.html',
+	styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-
+	menuList:MenuItem[] = [
+		{
+			text: "Kutyák",
+			icon: "fas fa-dog"
+		},
+		{
+			text: "Macskák",
+			icon: "fas fa-cat"
+		},
+		{
+			text: "Egerek",
+			icon: "fas fa-mouse"
+		},
+	]
 }
